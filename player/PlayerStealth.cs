00970c5bd6cda2efe2811d991c8e8e6884475999
Gustav0Damaceno <gustavohenrique-carvalho@hotmail.com>
2025-12-12 08:50:14 -0300

    Update PlayerStealth.cs

--- player/PlayerStealth.cs
+++ player/PlayerStealth.cs
@@ -2,12 +2,25 @@ using UnityEngine;
 
 public class PlayerStealth : MonoBehaviour
 {
-    PlayerMovement move;
-    
-    public bool IsStealthing => movement.isCrouching;
+    public bool isStealth = false;
+
+    PlayerCrouch crouch;
+    PlayerSprint sprint;
 
     void Start()
     {
-        move = GetComponent<PlayerMovement>();
+        crouch = GetComponent<PlayerCrouch>();
+        sprint = GetComponent<PlayerSprint>();
+    }
+
+    void Update()
+    {
+        isStealth = crouch.isCrouching && !sprint.isSprinting;
+    }
+
+    public float GetStealthModifier()
+    {
+        if (isStealth) return 0.5f;
+        return 1f;
     }
 }
